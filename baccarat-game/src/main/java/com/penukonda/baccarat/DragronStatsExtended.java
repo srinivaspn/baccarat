251721415459df6e80f3ddc6ceee589445d79aa7
Narayana Penukonda <solitaryarcher@gmail.com>
2020-03-27 17:29:04 -0700

    Added more  statistics

--- baccarat-game/src/main/java/com/penukonda/baccarat/DragronStatsExtended.java
+++ baccarat-game/src/main/java/com/penukonda/baccarat/DragronStatsExtended.java
@@ -15,7 +15,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-@WebServlet(name="DragonStatsNoReset", value="/dragonstatsextended")
+@WebServlet(name="DragronStatsExtended", value="/dragonstatsextended")
 public class DragronStatsExtended extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response)
